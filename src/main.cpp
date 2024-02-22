1c282c901be685b84175235bd384d19b70b7b26b
Tar1k13 <taras.grysjuk@gmail.com>
2024-02-22 22:16:34 +0200

    add movement functionality

--- src/main.cpp
+++ src/main.cpp
@@ -23,121 +23,145 @@ MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 #define BUTTON_PIN 2
 // Map led matrix column positions
 int mapping[15];
-void mapn(){
-  int counter=8;
-  for (size_t i = 0; i < 8; i++){
-    mapping[i]=counter++;
+void mapn() {
+  int counter = 8;
+  for (size_t i = 0; i < 8; i++) {
+    mapping[i] = counter++;
   }
-  counter=1;
-  for (size_t i = 9; i < 16; i++){
-    mapping[i]=counter++;
+  counter = 1;
+  for (size_t i = 9; i < 16; i++) {
+    mapping[i] = counter++;
   }
 }
-int buttonState=0;
-void control_listener(void *pvParameters){
-  while (true){
-    if(digitalRead(BUTTON_PIN)==LOW)
-      buttonState=1;
+int buttonStateLeft = 0;
+int buttonStateRight = 0;
+void control_listener(void *pvParameters) {
+  while (true) {
+    if (digitalRead(BUTTON_PIN) == LOW) buttonStateLeft = 1;
+    if (digitalRead(4) == LOW) buttonStateRight = 1;
     vTaskDelay(100 / portTICK_RATE_MS);
   }
-  
 }
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   mx.begin();
   tm.begin();
   mapn();
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
   xTaskCreate(control_listener, "calculate_water_temp", 2048, NULL, 2, NULL);
 }
 
+int figures[2][4][5] = {{{0x0, 0x0, 0x38, 0x10, 0x0},
+                         {0x0, 0x10, 0x30, 0x10, 0x0},
+                         {0x0, 0x0, 0x10, 0x38, 0x0},
+                         {0x0, 0x10, 0x38, 0x0, 0x0}},
+                        {{0x0, 0x4, 0x4, 0x4, 0x4},
+                         {0x0, 0x0, 0x3C, 0x0, 0x0},
+                         {0x0, 0x4, 0x4, 0x4, 0x4},
+                         {0x0, 0x0, 0x3C, 0x0, 0x0}}};
 
-int figures[2][4][5] = {
-  {
-    {0x0,0x0,0x1C,0x8,0x0},{0x0,0x8,0xC,0x8,0x0},{0x0,0x0,0x8,0x1C,0x0},{0x0,0x8,0x18,0x8,0x0}
-  },
-  {
-    {0x0,0x4,0x4,0x4,0x4},{0x0,0x0,0x3C,0x0,0x0},{0x0,0x4,0x4,0x4,0x4},{0x0,0x0,0x3C,0x0,0x0}
-  }
-};
-
-int startPos[2][4]={
-  {
-    2,1,2,1
-  },
-  {
-    1,2,1,2
-  }
-};
-
-
-
+int startPos[2][4] = {{1, 1, 2, 1}, {1, 2, 1, 2}};
 
 int matrix[16];
-int calculateHeight(int fig[]){
-  int counter=0;
-  for (size_t i = 0; i < sizeof(fig); i++){
-    if(fig[i]!=0) counter++;
+int calculateHeight(int fig[]) {
+  int counter = 0;
+  for (size_t i = 0; i <= sizeof(fig); i++) {
+    if (fig[i] != 0) counter++;
   }
   return counter;
 }
-int tst=0;
-void loop(){
-  int fig=0;
-  int rot =0;
-  boolean objectionX=false;
-  int heightX=calculateHeight(figures[fig][rot]);
 
+int countOneDigitsInHex(int hexNum) {
+  int count = 0;
 
-  int height=calculateHeight(figures[fig][rot]);
-  int currentCol=0;
-  boolean objection=false;
-    int curState=0;
-    for (size_t col = 0; col < 17-height; col++){
-      
-      if(buttonState==1) curState+=1;
-          for(int t=0;t<height;t++){
-            if((figures[fig][rot][startPos[fig][rot]+t]<<curState & matrix[col+t]) != 0){
-              Serial.println("OBJECTION");
-              // Serial.println()
-              objection=true;
-              break;
-            }
-          }
-        //moving blocks
-        if(!objection){
-            for(int t=0;t<height;t++){
-              int sum=(figures[fig][rot][startPos[fig][rot]+t]<<curState)+matrix[col+t]; //safe put
-              // if(mapping[col+t]!=8)
-              // Serial.println("col:");
-              // Serial.println(mapping[col+t]);
-              // Serial.println("val:");
-              // Serial.println(sum);
-                mx.setColumn(mapping[col+t],sum); //put
-              // Serial.println(sum);
-              // Serial.println(mapping[col-1]);
-              Serial.println(col);
-              if(col!=0)
-                mx.setColumn(mapping[col-1],matrix[col-1]); //move
-            }
-        } else break;
-      currentCol=col;
+  // Shift and mask to isolate each digit
+  for (int i = 0; i < 8; i++) {
+    if ((hexNum & (0xF << (4 * i))) == 0x1 << (4 * i)) {
+      count++;
+    }
+    hexNum >>= 4;  // Shift right by 4 bits
+  }
 
-      buttonState=0;
-      delay(1000);
+  return count;
+}
+
+void loop() {
+  int fig = 0;
+  int rot = 2;
+  boolean objectionX = false;
+  int heightX = calculateHeight(figures[fig][rot]);
+  // int width=countOnesInHexList(figures[fig][rot],5);
+
+  int height = calculateHeight(figures[fig][rot]);
+  int currentCol = 0;
+  boolean objection = false;
+  int curState = 0;
+  int curState1 = 0;
+  for (size_t col = 0; col < 17 - height; col++) {
+    // check boundaries
+
+    // left
+    if (buttonStateLeft == 1) {
+      boolean st = true;
+      for (int t = 0; t < height; t++) {
+        int elem = figures[fig][rot][startPos[fig][rot] + t] << curState + 1;
+        if (countOneDigitsInHex(elem) !=
+            countOneDigitsInHex(figures[fig][rot][startPos[fig][rot] + t]
+                                << curState)) {
+          st = false;
+          break;
+        }
+      }
+      if (st) curState += 1;
+    }
+    // right
+    if (buttonStateRight == 1) {
+      boolean st = true;
+      for (int t = 0; t < height; t++) {
+        int elem = figures[fig][rot][startPos[fig][rot] + t] >> curState1 + 1;
+        if (countOneDigitsInHex(elem) !=
+            countOneDigitsInHex(figures[fig][rot][startPos[fig][rot] + t] >>
+                                curState1)) {
+          st = false;
+          break;
+        }
+      }
+      if (st) curState1 += 1;
+    }
+    // main part
+    for (int t = 0; t < height; t++) {
+      int elem =
+          figures[fig][rot][startPos[fig][rot] + t] << curState >> curState1;
+      if ((elem & matrix[col + t]) != 0) {
+        Serial.println("OBJECTION");
+        // Serial.println()
+        objection = true;
+        break;
+      }
+    }
+    // moving blocks
+    if (!objection) {
+      for (int t = 0; t < height; t++) {
+        int elem = (figures[fig][rot][startPos[fig][rot] + t] << curState >>
+                    curState1) +
+                   matrix[col + t];
+        // safe put
+        mx.setColumn(mapping[col + t], elem);                           // put
+        if (col != 0) mx.setColumn(mapping[col - 1], matrix[col - 1]);  // move
+      }
+    } else
+      break;
+    currentCol = col;
+
+    buttonStateLeft = 0;
+    buttonStateRight = 0;
+    delay(200);
   }
-  for(int t=0;t<height;t++){
-    int sum=(figures[fig][rot][startPos[fig][rot]+t]<<curState)+matrix[currentCol+t];
-    matrix[currentCol+t]=sum;
-    // Serial.println(matrix[currentCol-t]);
+  for (int t = 0; t < height; t++) {
+    int sum =
+        (figures[fig][rot][startPos[fig][rot] + t] << curState >> curState1) +
+        matrix[currentCol + t];
+    matrix[currentCol + t] = sum;
   }
-  Serial.println();
-  for(int t=0;t<16;t++){
-    
-    Serial.println(matrix[t]);
-    
-    // Serial.println(matrix[currentCol-t]);
-  }
-  Serial.println();
 }
