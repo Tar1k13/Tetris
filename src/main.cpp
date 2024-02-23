5e6deb5daffb0871ce77543febbcc038667249c8
Tar1k13 <taras.grysjuk@gmail.com>
2024-02-23 22:03:25 +0200

    Rotation support [refactor required]

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <SPI.h>
 #include <TM1637.h>
 
-using namespace std;
+// using namespace std;
 
 TM1637 tm(25, 26);
 
@@ -35,10 +35,12 @@ void mapn() {
 }
 int buttonStateLeft = 0;
 int buttonStateRight = 0;
+int buttonStateFlip = 0;
 void control_listener(void *pvParameters) {
   while (true) {
     if (digitalRead(BUTTON_PIN) == LOW) buttonStateLeft = 1;
     if (digitalRead(4) == LOW) buttonStateRight = 1;
+    if (digitalRead(16) == LOW) buttonStateFlip = 1;
     vTaskDelay(100 / portTICK_RATE_MS);
   }
 }
@@ -47,8 +49,9 @@ void setup() {
   mx.begin();
   tm.begin();
   mapn();
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);  // left button
+  pinMode(4, INPUT_PULLUP);           // right button
+  pinMode(16, INPUT_PULLUP);
   xTaskCreate(control_listener, "calculate_water_temp", 2048, NULL, 2, NULL);
 }
 
@@ -75,20 +78,38 @@ int calculateHeight(int fig[]) {
 int countOneDigitsInHex(int hexNum) {
   int count = 0;
 
-  // Shift and mask to isolate each digit
-  for (int i = 0; i < 8; i++) {
-    if ((hexNum & (0xF << (4 * i))) == 0x1 << (4 * i)) {
-      count++;
-    }
-    hexNum >>= 4;  // Shift right by 4 bits
+  // Method 1: Bitwise approach (optimized for Arduino)
+  for (int i = 0; i < 8; i++) {  // Iterate through 8 digits
+    count += (hexNum & 1);       // Add 1 if least significant bit is 1
+    hexNum >>= 1;                // Shift number right by 1 bit
   }
 
   return count;
 }
 
+int countZeroBits(int num, int numBits) {
+  int count = 0;
+  for (int i = 0; i < numBits; i++) {
+    if ((num & (1 << i)) == 0) {
+      count++;
+    }
+  }
+  return count;
+}
+int countOnes(int num, int numBits) {
+  int count = 0;
+  for (int i = 0; i < numBits; i++) {
+    // Check if bit is set (1) using bitwise AND
+    if (num & (1 << i)) {
+      count++;
+    }
+  }
+  return count;
+}
+
 void loop() {
-  int fig = 0;
-  int rot = 2;
+  int fig = 1;
+  int rot = 0;
   boolean objectionX = false;
   int heightX = calculateHeight(figures[fig][rot]);
   // int width=countOnesInHexList(figures[fig][rot],5);
@@ -99,6 +120,36 @@ void loop() {
   int curState = 0;
   int curState1 = 0;
   for (size_t col = 0; col < 17 - height; col++) {
+    // rotate figures
+    if (buttonStateFlip == 1) {
+      int newHeight = calculateHeight(figures[fig][rot+1]);
+      boolean freeSpace=true;
+      //TODO: check rotate ability
+      for (int t = 0; t < newHeight; t++) {
+         int elem = (figures[fig][rot][startPos[fig][rot] + t] << curState >>
+                    curState1) +
+                   matrix[col + t];
+        // safe put
+        while (countOnes(elem, 8) < countOnes(elem, 12)) {
+          elem = (elem >> 1) + matrix[col+t];
+        }
+
+        if(matrix[col+t]!=0 && (matrix[col+t] | elem) == elem){
+          freeSpace=false;
+          break;
+        };
+      }
+      if(freeSpace){
+        for (int i = 0; i < height; i++) {
+          // Serial.println(col+i);
+          mx.setColumn(mapping[col + i], matrix[col + i]);
+        }
+        rot += 1;
+        // int newHeight = calculateHeight(figures[fig][rot]);
+        height = newHeight;
+      }
+    }
+
     // check boundaries
 
     // left
@@ -118,7 +169,7 @@ void loop() {
     // right
     if (buttonStateRight == 1) {
       boolean st = true;
-      for (int t = 0; t < height; t++) {
+      for (int t = 0; t <= height; t++) {
         int elem = figures[fig][rot][startPos[fig][rot] + t] >> curState1 + 1;
         if (countOneDigitsInHex(elem) !=
             countOneDigitsInHex(figures[fig][rot][startPos[fig][rot] + t] >>
@@ -133,6 +184,9 @@ void loop() {
     for (int t = 0; t < height; t++) {
       int elem =
           figures[fig][rot][startPos[fig][rot] + t] << curState >> curState1;
+      while (countOnes(elem, 8) < countOnes(elem, 12)) {
+        elem = (elem >> 1) + matrix[col+t];
+      }
       if ((elem & matrix[col + t]) != 0) {
         Serial.println("OBJECTION");
         // Serial.println()
@@ -147,21 +201,29 @@ void loop() {
                     curState1) +
                    matrix[col + t];
         // safe put
+        while (countOnes(elem, 8) < countOnes(elem, 12)) {
+          elem = (elem >> 1) + matrix[col+t];
+        }
         mx.setColumn(mapping[col + t], elem);                           // put
         if (col != 0) mx.setColumn(mapping[col - 1], matrix[col - 1]);  // move
       }
+      Serial.println("------");
     } else
       break;
     currentCol = col;
 
     buttonStateLeft = 0;
     buttonStateRight = 0;
-    delay(200);
+    buttonStateFlip = 0;
+    delay(1000);
   }
   for (int t = 0; t < height; t++) {
     int sum =
         (figures[fig][rot][startPos[fig][rot] + t] << curState >> curState1) +
         matrix[currentCol + t];
+    while (countOnes(sum, 8) < countOnes(sum, 12)) {
+      sum = (sum >> 1) + matrix[currentCol + t];
+    }
     matrix[currentCol + t] = sum;
   }
 }
