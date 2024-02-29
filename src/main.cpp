382e1382583c3e5d827223c3c57b5acfa69442c1
Tar1k13 <taras.grysjuk@gmail.com>
2024-02-29 22:27:35 +0200

    Concept changes

--- src/main.cpp
+++ src/main.cpp
@@ -33,13 +33,13 @@ void mapn() {
 // int buttonStateRight = 0;
 // int buttonStateFlip = 0;
 
-int figures[7][4][5] = {{{0x0, 0x0, 0x38, 0x10, 0x0},  // T
+int figures[7][4][5] = {{{0x0, 0x10, 0x18, 0x10, 0x0},  // T
+                         {0x0, 0x0, 0x38, 0x10, 0x0},
                          {0x0, 0x10, 0x30, 0x10, 0x0},
-                         {0x0, 0x0, 0x10, 0x38, 0x0},
-                         {0x0, 0x10, 0x18, 0x10, 0x0}},
-                        {{0x0, 0x4, 0x4, 0x4, 0x4},  // I
+                         {0x0, 0x10, 0x38, 0x0, 0x0}},
+                        {{0x0, 0x8, 0x8, 0x8, 0x8},  // I
                          {0x0, 0x0, 0x3C, 0x0, 0x0},
-                         {0x0, 0x4, 0x4, 0x4, 0x4},
+                         {0x0, 0x8, 0x8, 0x8, 0x8},
                          {0x0, 0x0, 0x3C, 0x0, 0x0}},
                         {{0x0, 0x0, 0x18, 0x18, 0x0},  // O
                          {0x0, 0x0, 0x18, 0x18, 0x0},
@@ -62,7 +62,7 @@ int figures[7][4][5] = {{{0x0, 0x0, 0x38, 0x10, 0x0},  // T
                          {0x0, 0x8, 0x18, 0x10, 0x0},
                          {0x0, 0x18, 0xC, 0x0, 0x0}}};
 
-int startPos[7][4] = {{2, 1, 2, 1},   // T
+int startPos[7][4] = {{1, 2, 1, 1},   // T
                       {1, 2, 1, 2},   // I
                       {2, 2, 2, 2},   // O
                       {1, 2, 1, 1},   // L
@@ -113,79 +113,109 @@ int countOnes(int num, int numBits) {
 
 int curState = 0;       // current left move
 int curState1 = 0;      // current right move
-int fig = 1;            // current figure
+int fig = 3;            // current figure
 int rot = 0;            // current rotation
 int currentColumn = 0;  // current column
 int height = 0;         // current height
 int mv = 0;             // current rotation mudslide due to rotation bondaries
 int ml = 0;
+int currentfigure[5];
 
-boolean checkObjection(int col, int height) {
-  for (int t = 0; t < height; t++) {
-    int elem = figures[fig][rot][startPos[fig][rot] + t] << curState >>
-               curState1 >> mv;
-    if ((elem & matrix[col + t]) != 0) {
+boolean checkObjection(int col) {
+  int bias=0;
+  for(int i=startPos[fig][rot];i<startPos[fig][rot]+height;i++){
+    int elem=currentfigure[i];
+    if ((elem & matrix[col + bias]) != 0) {
       Serial.println("OBJECTION");
       return true;
     }
+    bias++;
   }
   return false;
 }
 
-boolean checkBoundings(int height, boolean right) {
+boolean checkBoundings(boolean right) {
   if (!right) {
-    for (int t = 0; t < height; t++) {
-      int elem = figures[fig][rot][startPos[fig][rot] + t] << curState + 1 >>
-                 curState1 >> mv;
-      int curElem = figures[fig][rot][startPos[fig][rot] + t] << curState >>
-                    curState1 >> mv;
-      if (countOneDigitsInHex(elem) != countOneDigitsInHex(curElem)) {
-        return false;
+    int bias=0;
+      for(int i=startPos[fig][rot];i<startPos[fig][rot]+height;i++){
+        int elem=currentfigure[i]<<1;
+        if (countOneDigitsInHex(elem) != countOneDigitsInHex(currentfigure[i])) {
+          return false;
+        }
+        bias++;
       }
-    }
-    return true;
+      return true;
+    // for (int t = 0; t < height; t++) {
+    //   int elem = figures[fig][rot][startPos[fig][rot] + t] << curState + 1 >>
+    //              curState1 >> mv;
+    //   int curElem = figures[fig][rot][startPos[fig][rot] + t] << curState >>
+    //                 curState1 >> mv;
+    //   if (countOneDigitsInHex(elem) != countOneDigitsInHex(curElem)) {
+    //     return false;
+    //   }
+    // }
+    // return true;
   } else {
-    for (int t = 0; t < height; t++) {
-      int elem = figures[fig][rot][startPos[fig][rot] + t] << curState >>
-                 (curState1 + 1) >> mv;
-      int curElem = figures[fig][rot][startPos[fig][rot] + t] << curState >>
-                    curState1 >> mv;
-      // curElem=curElem>>mv;
-      // Serial.println(elem);
-      if (elem == 0 ||
-          (countOneDigitsInHex(elem) != countOneDigitsInHex(curElem))) {
-        return false;
+        int bias=0;
+      for(int i=startPos[fig][rot];i<startPos[fig][rot]+height;i++){
+        int elem=currentfigure[i]>>1;
+        if (countOneDigitsInHex(elem) != countOneDigitsInHex(currentfigure[i])) {
+          return false;
+        }
+        bias++;
       }
-    }
-    return true;
+      return true;
   }
   return true;
 }
 
-void move(int col, int height, int prevStep, int nextStep, boolean right) {
-  if (right)
-    curState1 = nextStep;
-  else
-    curState = nextStep;
-  if (!checkObjection(col, height)) {
-    for (int t = 0; t < height; t++) {
-      int elem = ((figures[fig][rot][startPos[fig][rot] + t] << curState >>
-                   curState1 >> mv) +
-                  matrix[col + t]);
-      // safe put
-      while (countOnes(elem, 8) < countOnes(elem, 12)) {
-        elem = (elem >> 1) + matrix[col + t];
-      }
+void move(int col, int prevStep, int nextStep, boolean right) {
+  if(right){
+    Serial.println("HER");
 
-      mx.setColumn(mapping[col + t], elem);
+    if(!checkObjection(col)){
+       int bias=0;
+      for(int i=startPos[fig][rot];i<startPos[fig][rot]+height;i++){
+        currentfigure[i]=(currentfigure[i]>>1);
+        int elem=currentfigure[i]+matrix[col+bias];
+        mx.setColumn(mapping[col + bias], elem);
+        bias++;
+      }
     }
   } else {
-    Serial.println("HER");
-    if (right)
-      curState1 = prevStep;
-    else
-      curState = prevStep;
+    int bias=0;
+    if(!checkObjection(col)){
+      for(int i=startPos[fig][rot];i<startPos[fig][rot]+height;i++){
+        currentfigure[i]=currentfigure[i]<<1;
+        int elem=currentfigure[i]+matrix[col+bias];
+        mx.setColumn(mapping[col + bias], elem);
+        bias++;
+      }
+    }
   }
+  // if (right)
+  //   curState1 = nextStep;
+  // else
+  //   curState = nextStep;
+  // if (!checkObjection(col, height)) {
+  //   for (int t = 0; t < height; t++) {
+  //     int elem = ((figures[fig][rot][startPos[fig][rot] + t] << curState >>
+  //                  curState1 >> mv) +
+  //                 matrix[col + t]);
+  //     // safe put
+  //     while (countOnes(elem, 8) < countOnes(elem, 12)) {
+  //       elem = (elem >> 1) + matrix[col + t];
+  //     }
+
+  //     mx.setColumn(mapping[col + t], elem);
+  //   }
+  // } else {
+  //   Serial.println("HER");
+  //   if (right)
+  //     curState1 = prevStep;
+  //   else
+  //     curState = prevStep;
+  // }
 }
 
 // boolean checkRotateObjection(int col,int fg,int rt){
@@ -200,54 +230,64 @@ void move(int col, int height, int prevStep, int nextStep, boolean right) {
 //   return false;
 // }
 
-boolean isRotateAble(int col,int fg,int rt){
-  int newHeight = calculateHeight(figures[fg][rt]);
-  Serial.print(col);
-  Serial.print(" ");
-  Serial.println(newHeight);
-  Serial.println(col+newHeight);
-  if((col+newHeight) > 16) return false;
-   for (int t = 0; t < newHeight; t++) {
-    // put
-    int elem = ((figures[fg][rt][startPos[fg][rt] + t] << curState >>
-                 curState1 >> mv));
-    while (countOnes(elem, 8) < countOnes(elem, 12)) {
-      elem = (elem >> 1);
-    }
-    if((elem & matrix[col+t]) != 0){
-      return false;
-    }
-   }
-  return true;;
+boolean isRotateAble(int rotated,int input){
+  if((rotated & input)!=0)
+    return false;
+  return true;
 }
-void rotate(int col, int heigh) {
-  if(!isRotateAble(currentColumn,fig,rot+1)) return;
-  for (int t = 0; t < heigh; t++) {
-    // clear
-    mx.setColumn(mapping[col + t], matrix[col + t]);
+int rotate(int col, int heigh,int mvr) { //returns right-left movement factor
+  int bias=0;
+  for(int i=startPos[fig][rot];i<startPos[fig][rot]+height;i++){
+    mx.setColumn(mapping[col+bias],matrix[col+bias]);
+    currentfigure[i]=0x0;
+    bias++;
   }
+  
   if (rot < 3){
     rot += 1;
   }
   else{
     rot = 0;
   }
-  
-  int newHeight = calculateHeight(figures[fig][rot]);
-  height = newHeight;
-  for (int t = 0; t < newHeight; t++) {
-    // put
-    int elem = ((figures[fig][rot][startPos[fig][rot] + t] << curState >>
-                 curState1 >> mv) +
-                matrix[col + t]);
-    while (countOnes(elem, 8) < countOnes(elem, 12)) {
-      elem = (elem >> 1) + matrix[col + t];
-      mv++;
-      // rotateBias++;
+  //put
+  height=calculateHeight(figures[fig][rot]);
+  int sm=0; //required to bound bias due to limit rotate
+  for(int i=startPos[fig][rot];i<startPos[fig][rot]+height;i++){
+    int inElem=figures[fig][rot][i];
+    if(mv>=0){
+      int elem=figures[fig][rot][i] >> (mvr);
+      for(int t=sm;t<0;t++){
+        elem=(elem<<1) | 1;
+      }
+      while(countOnes(inElem,8)!=countOnes(elem,8)){
+        elem=(elem<<1) | 1;
+        sm--;
+      }
+      currentfigure[i]=elem;
     }
-    // Serial.println(elem);
-    mx.setColumn(mapping[col + t], elem);
+    else{
+      int elem=(figures[fig][rot][i] << (mvr*(-1)));
+
+    //   Serial.println(elem);
+      while(countOnes(inElem,8)!=countOnes(elem,8)){
+        elem=elem>>1;
+        sm++;
+      }
+      currentfigure[i]=(figures[fig][rot][i] << (mvr*(-1))) >> sm;
+    // }
+      
+ 
   }
+  }
+  bias=0;
+  Serial.println(startPos[0][0]);
+  for(int i=startPos[fig][rot];i<startPos[fig][rot]+height;i++){
+    int elem=currentfigure[i]+matrix[col+bias];
+    // Serial.println(currentfigure[i]);
+    mx.setColumn(mapping[col+bias],elem);
+    bias++;
+  }
+  return sm;
 }
 
 // one-click flags
@@ -256,29 +296,33 @@ boolean flag2 = false;
 boolean flag3 = false;
 
 // input listener
+
 void control_listener(void *pvParameters) {
+
   while (true) {
     int height = calculateHeight(figures[fig][rot]);
 
     // left
-    if (digitalRead(BUTTON_PIN) == LOW && checkBoundings(height, false) &&
-        !flag1) {
+    if (digitalRead(BUTTON_PIN) == LOW &&
+        !flag1 && checkBoundings(false)) {
       flag1 = true;
-      move(currentColumn, height, curState, curState + 1, false);
+      move(currentColumn, curState, curState + 1, false);
+      mv--;
     }
     if (digitalRead(BUTTON_PIN) == HIGH) flag1 = false;
 
     // right
-    if (digitalRead(4) == LOW && checkBoundings(height, true) && !flag2) {
+    if (digitalRead(4) == LOW && !flag2 && checkBoundings(true)) {
       flag2 = true;
-      move(currentColumn, height, curState1, curState1 + 1, true);
+      move(currentColumn, curState1, curState1 + 1, true);
+      mv++;
     }
-    if (digitalRead(4) == HIGH) flag2 = false;
+    if (digitalRead(4) == HIGH ) flag2 = false;
 
     // rotate
     if (digitalRead(16) == LOW && !flag3) {
       flag3 = true;
-      rotate(currentColumn, height);
+      mv+=rotate(currentColumn, height,mv);
     }
     if (digitalRead(16) == HIGH) flag3 = false;
     vTaskDelay(50 / portTICK_RATE_MS);
@@ -290,6 +334,9 @@ void setup() {
   mx.begin();
   tm.begin();
   mapn();
+  // for(int i=0;i<5;i++){
+  //   currentfigure[i]=figures[fig][rot][i];
+  // }
   pinMode(BUTTON_PIN, INPUT_PULLUP);  // left button
   pinMode(4, INPUT_PULLUP);           // right button
   pinMode(16, INPUT_PULLUP);
@@ -297,74 +344,92 @@ void setup() {
 }
 
 void loop() {
-  fig=rand() % 7;
-  rot=rand() % 3;
-  boolean objectionX = false;
-  int heightX = calculateHeight(figures[fig][rot]);
+  // fig=rand() % 7;
+  // rot=rand() % 3;
+
+  for(int i=0;i<5;i++){
+    currentfigure[i]=figures[fig][rot][i];
+  }
+
   // int width=countOnesInHexList(figures[fig][rot],5);
 
-  height = calculateHeight(figures[fig][rot]);
+  height = calculateHeight(currentfigure);
   boolean objection = false;
 
   int rotateBias = 0;
   int currentCol = 0;
-
+  
   for (size_t col = 0; col < 17 - height; col++) {
     currentColumn = col;
-    objection = checkObjection(col, height);
+    // objection = checkObjection(col, height);
 
-    if (!objection) {
-      for (int t = 0; t < height; t++) {
-        int elem = ((figures[fig][rot][startPos[fig][rot] + t] << curState >>
-                     curState1 >> mv) +
-                    matrix[col + t]);
-        // Serial.println(elem);
-        mx.setColumn(mapping[col + t], elem);                           // put
-        if (col != 0) mx.setColumn(mapping[col - 1], matrix[col - 1]);  // move
+    if (!checkObjection(col)) {
+      int bias=0;
+      for(int i=startPos[fig][rot];i<startPos[fig][rot]+height;i++){
+        int elem=currentfigure[i]+matrix[col+bias];
+        mx.setColumn(mapping[col+bias],elem);
+        if(col!=0) mx.setColumn(mapping[col - 1], matrix[col - 1]);
+        bias++;
       }
+      // for (int t = 0; t < height; t++) {
+      //   int elem = ((figures[fig][rot][startPos[fig][rot] + t] << curState >>
+      //                curState1 >> mv) +
+      //               matrix[col + t]);
+      //   // Serial.println(elem);
+      //   mx.setColumn(mapping[col + t], elem);                           // put
+      //   if (col != 0) mx.setColumn(mapping[col - 1], matrix[col - 1]);  // move
+      // }
       // Serial.println();
-    } else
-      break;
+    // } 
+    // else
+    //   break;
     currentCol = col;
 
-    delay(500);
+    delay(1000);
   }
-  for (int t = 0; t < height; t++) {
-    int sum = (figures[fig][rot][startPos[fig][rot] + t] << curState >>
-               curState1 >> mv) +
-              matrix[currentCol + t];
-    while (countOnes(sum, 8) < countOnes(sum, 12)) {
-      sum = (sum >> 1) + matrix[currentCol + t];
-    }
-    matrix[currentCol + t] = sum;
   }
+  int bias=0;
+   for(int i=startPos[fig][rot];i<startPos[fig][rot]+height;i++){
+        int elem=currentfigure[i]+matrix[currentCol+bias];
+        matrix[currentCol+bias]=elem;
+        Serial.println(currentCol+bias);
+        bias++;
+  }
+
+  // for (int t = 0; t < height; t++) {
+  //   int sum = (figures[fig][rot][startPos[fig][rot] + t] << curState >>
+  //              curState1 >> mv) +
+  //             matrix[currentCol + t];
+  //   while (countOnes(sum, 8) < countOnes(sum, 12)) {
+  //     sum = (sum >> 1) + matrix[currentCol + t];
+  //   }
+  //   matrix[currentCol + t] = sum;
+  // }
   //delete line
-  for(int i=0;i<(sizeof(matrix)/sizeof(matrix[0]));i++){
-    if(countOnes(matrix[i],8)==8){
-      //animation
-      int ts=matrix[i];
-      for(int t=2;t<=4;t++){
-        ts=ts>>t;
-        ts=ts<<t-1;
-        // ts=ts<<i+1;
-        mx.setColumn(mapping[i],ts);
-        delay(500);
-      }
-      matrix[i]=0;
-      for(int t=i;t>0;t--){
-        matrix[t]=matrix[t-1];
-      }
-    };
-  }
-  // //re-render
-  for(int i=0;i<(sizeof(matrix)/sizeof(matrix[0]))-1;i++){
-    mx.setColumn(mapping[i],matrix[i]);
-  }
-  Serial.println();
-  curState = 0;
-  curState1 = 0;
+  // for(int i=0;i<(sizeof(matrix)/sizeof(matrix[0]));i++){
+  //   if(countOnes(matrix[i],8)==8){
+  //     //animation
+  //     int ts=matrix[i];
+  //     for(int t=2;t<=4;t++){
+  //       ts=ts>>t;
+  //       ts=ts<<t-1;
+  //       // ts=ts<<i+1;
+  //       mx.setColumn(mapping[i],ts);
+  //       delay(500);
+  //     }
+  //     matrix[i]=0;
+  //     for(int t=i;t>0;t--){
+  //       matrix[t]=matrix[t-1];
+  //     }
+  //   };
+  // }
+  // // //re-render
+  // for(int i=0;i<(sizeof(matrix)/sizeof(matrix[0]))-1;i++){
+  //   mx.setColumn(mapping[i],matrix[i]);
+  // }
+  // Serial.println();
+  // curState = 0;
+  // curState1 = 0;
   rot = 0;
   mv = 0;
 }
-
-// problem with objection(round case)
